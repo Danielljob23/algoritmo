576cc0b8c960a818fbe323203e3d72fddacbcc04
Danielljob23 <126108631+Danielljob23@users.noreply.github.com>
2024-09-13 18:23:43 -0300

    Delete main directory

--- main/src/br/inatel/cdg/po/HardwareBasico.java
+++
@@ -1,7 +0,0 @@
-package br.inatel.cdg.po;
-
-public class HardwareBasico {
-
-     String nome;
-     float capacidade;
-}
--- main/src/br/inatel/cdg/po/SistemaOperacional.java
+++
@@ -1,7 +0,0 @@
-package br.inatel.cdg.po;
-
-public class SistemaOperacional {
-
-    String nome;
-    int tipo;
-}
